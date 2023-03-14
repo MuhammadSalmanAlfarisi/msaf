e783970d5d3cedfe3ff5e7c77639da9fa4ee985b
salman261002 <116475964+MuhammadSalmanAlfarisi@users.noreply.github.com>
2023-03-15 03:09:05 +0700

    feats: nitpicks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,12 +16,14 @@ const Home = () => {
 
       {/* main home */}
       <div className="z-10 flex flex-col items-center p-3 text-center ">
-        <img src={Me} alt="" className="mb-3 w-36 lg:w-44 rounded-[40%]" />
+        <img src={Me} alt="" className="mb-3 w-36 rounded-[40%] lg:w-44" />
         <h1 className="text-[36px] font-bold lg:text-[42px]">
           Salman Alfarisi
         </h1>
         {/* add animation type */}
-        <span className="text-md font-semibold">Frontend Dev</span>
+        <span className="text-md font-semibold">
+          <code>Frontend Dev</code>
+        </span>
         <Social />
         <a href="#about" className="absolute bottom-16">
           <CgScrollV className="ani-mouse relative h-8 w-8 hover:text-first" />
