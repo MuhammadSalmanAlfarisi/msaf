a80e8d1eae3d0678b241380e384e3eb509c59eb6
salman261002 <116475964+MuhammadSalmanAlfarisi@users.noreply.github.com>
2023-03-15 05:55:09 +0700

    feats: layout fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,22 @@
 module.exports = {
   content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   theme: {
+    screens: {
+      'sm': '700px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '800px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1200px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1400px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       colors: {
         first: "#ff4d61",
